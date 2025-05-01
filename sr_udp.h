cfd107ca472084ddfe76e823de8387d48a39da0c
saratrava <65900615+saratrava@users.noreply.github.com>
2025-05-01 18:37:17 +0200

    Delete sr_udp.h

--- sr_udp.h
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <errno.h>
-#include <fcntl.h>
-#include <unistd.h>
-
-void file_transfer_debug(conn_arg args, int sd, struct sockaddr_in server, char *func, char *path);
